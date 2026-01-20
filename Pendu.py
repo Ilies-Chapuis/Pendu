4664e32887cd827a1be37f6dbcbe4040fdf8586e
Ilies-Chapuis <ilies.chapuis@laplateforme.io>
2026-01-20 08:57:22 +0100

    Def Score

---
+++ Pendu.py
@@ -0,0 +1,6 @@
+import pygame
+
+def score(HighScore, RecentScore):
+    print("Look at your high score : 1")
+    print("Look at your recent scores : 2")
+    Choice = input("Choose between the 2 choices : ")
