a0be57f868837e9f337d86bdb7f414343e5b4a84
Ilies-Chapuis <ilies.chapuis@laplateforme.io>
2026-01-20 09:01:32 +0100

    Choice Options Added

--- Pendu.py
+++ Pendu.py
@@ -4,3 +4,8 @@ def score(HighScore, RecentScore):
     print("Look at your high score : 1")
     print("Look at your recent scores : 2")
     Choice = input("Choose between the 2 choices : ")
+
+    if Choice == "1":
+
+
+    elif Choice == "2":
