981790f7199933b6e75cdd32f905276fe456660f
Ilies-Chapuis <ilies.chapuis@laplateforme.io>
2026-01-20 11:28:39 +0100

    Imported Pygame

--- Pendu.py
+++ Pendu.py
@@ -1,17 +1,22 @@
-def score():
+import pygame
+from pygame.locals import*
+
+
+def scoremenu():
     print("Look at your high score : 1")
     print("Look at your recent scores : 2")
     Choice = input("Choose between the 2 choices : ")
 
-    if Choice == "1":
+    if Choice == K_1:
         highscore()
 
-
-    elif Choice == "2":
+    elif Choice == K_2:
         recentscores()
 
 
 def highscore():
+    with open("scores.txt", "r", encoding="utf-8"):
 
 
 def recentscores():
+    with open("scores.txt", "r", encoding="utf-8"):
