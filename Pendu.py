7b3a58f718168639ec8c4d7a43b281d8bf48eea1
Ilies-Chapuis <ilies.chapuis@laplateforme.io>
2026-01-20 09:58:40 +0100

    Added more Def, Changed Layout

--- Pendu.py
+++ Pendu.py
@@ -1,11 +1,17 @@
-import pygame
-
-def score(HighScore, RecentScore):
+def score():
     print("Look at your high score : 1")
     print("Look at your recent scores : 2")
     Choice = input("Choose between the 2 choices : ")
 
     if Choice == "1":
+        highscore()
 
 
     elif Choice == "2":
+        recentscores()
+
+
+def highscore():
+
+
+def recentscores():
